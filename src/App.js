8ae707d1f7ddb0c96ae4d4acc96a1ad6a68839f6
Nikita Kadam <kadamnik68@gmail.com>
2025-01-27 13:23:59 +0530

    initial commit-Coffee Customization

--- src/App.js
+++ src/App.js
@@ -2,30 +2,42 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [distance, setDistance] = useState();
-  const [result, setResult] = useState();
-  const onTranspotClick = () => {
-    if (distance < 3) {
-      setResult("walk")
+  const [size, setSize] = useState();
+  const [extrashot, setExtrashot] = useState();
+  const [check, setCheck] = useState();
 
-    }
-    else if (distance >= 3 && distance <= 15) {
-      setResult("Bike")
-
-    }
-    else if (distance > 15) {
-      setResult("Car")
+  const onCustomizeClick = () => {
+    if (size === "small" && extrashot === "order") {
+      setCheck("Small with an option for an extra shot of espresso");
+    } else if (size === "medium" && extrashot === "order") {
+      setCheck("Medium with an option for an extra shot of espresso");
+    } else if (size === "large" && extrashot === "order") {
+      setCheck("Large with an option for an extra shot of espresso");
+    } else {
+      setCheck("Please select a valid size and option for an extra shot");
     }
   }
-  const mode = (event) => {
-    setDistance(event.target.value);
+  const customerOrder = (event) => {
+    setSize(event.target.value);
+  }
+
+  const order = (event) => {
+    setExtrashot(event.target.value);
+
   }
   return (
     <div>
-      <h1>Transportation Mode Selection</h1>
-      <input type="text" value={distance} onChange={mode} />
-      <button onClick={onTranspotClick}>check</button>
-      <p>{result}</p>
+      <h1>Coffee Custamization</h1>
+      <select value={size} onChange={customerOrder}>
+        <option value={""}>select</option>
+        <option value={"small"}>Small</option>
+        <option value={"medium"}>Medium</option>
+        <option value={"large"}>Large</option>
+      </select>
+      <input type="text" value={extrashot} onChange={order} />
+      <button onClick={onCustomizeClick}>check</button>
+      <p>{check}</p>
+
 
     </div>
   );
