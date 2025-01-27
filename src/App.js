8690d12f14bdbbcc1f94fc7c33941c1658253994
Nikita Kadam <kadamnik68@gmail.com>
2025-01-27 14:23:50 +0530

    initial commit-password strength checker

--- src/App.js
+++ src/App.js
@@ -2,43 +2,28 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [size, setSize] = useState();
-  const [extrashot, setExtrashot] = useState();
+  const [password, setPassword] = useState();
   const [check, setCheck] = useState();
-
-  const onCustomizeClick = () => {
-    if (size === "small" && extrashot === "order") {
-      setCheck("Small with an option for an extra shot of espresso");
-    } else if (size === "medium" && extrashot === "order") {
-      setCheck("Medium with an option for an extra shot of espresso");
-    } else if (size === "large" && extrashot === "order") {
-      setCheck("Large with an option for an extra shot of espresso");
-    } else {
-      setCheck("Please select a valid size and option for an extra shot");
+  const onPasswordCheckClick = () => {
+    if (password.length < 6) {
+      setCheck("Weak");
+    }
+    else if (password.length >= 6 && password.length <= 10) {
+      setCheck("Medium");
+    }
+    else {
+      setCheck("strong");
     }
   }
-  const customerOrder = (event) => {
-    setSize(event.target.value);
-  }
-
-  const order = (event) => {
-    setExtrashot(event.target.value);
-
+  const strength = (event) => {
+    setPassword(event.target.value);
   }
   return (
     <div>
-      <h1>Coffee Custamization</h1>
-      <select value={size} onChange={customerOrder}>
-        <option value={""}>select</option>
-        <option value={"small"}>Small</option>
-        <option value={"medium"}>Medium</option>
-        <option value={"large"}>Large</option>
-      </select>
-      <input type="text" value={extrashot} onChange={order} />
-      <button onClick={onCustomizeClick}>check</button>
+      <h1>Password Strength checker</h1>
+      <input type="text" value={password} onChange={strength} />
+      <button onClick={onPasswordCheckClick}>Check</button>
       <p>{check}</p>
-
-
     </div>
   );
 }
